35984e0d5e8813369e464b6b8fc9c8e2a84809a4
tevoran <j.pfeiffer93@gmx.de>
2021-11-21 15:46:37 +0100

    a first test triangle

--- src/engine/gfx/renderer/render.c
+++ src/engine/gfx/renderer/render.c
@@ -1,7 +1,32 @@
 #include <tt.h>
 
 
+//test data
+GLfloat tri[]=
+{
+	-1.0, 1.0, 0.0,
+	1.0, 1.0, 0.0,
+	0.0, -1.0, 0.0,
+};
+
 void tt_gfx_render()
 {
-	printf("render\n");
+	GLuint vao;
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+
+	GLuint vbo;
+	glGenBuffers(1, &vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(tri), tri, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(GLfloat)*3, (void*)0);
+	glEnableVertexAttribArray(0);
+
+	glDrawArrays(GL_TRIANGLES, 0, 3);
+	//printf("%i\n", sizeof(tri));
+
+
+	glDeleteBuffers(1, &vbo);
+	glDeleteVertexArrays(1, &vao);
 }
--- src/test.c
+++ src/test.c
@@ -4,8 +4,11 @@ int main()
 {
 	tt_init("T3Vtech3 test window", 1366, 768, false);
 
-	tt_new_frame();
-	SDL_Delay(3000);
+	for(int i=0; i<1000; i++)
+	{
+		tt_new_frame();		
+	}
+	//SDL_Delay(3000);
 
 
 	return 0;
