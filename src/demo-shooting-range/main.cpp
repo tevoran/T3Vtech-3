a2ab27a1bdc45b9167c0c025b5f6e4fff9a30002
tevoran <j.pfeiffer93@gmx.de>
2021-12-27 00:04:17 +0100

    add demo video

--- src/demo-shooting-range/main.cpp
+++ src/demo-shooting-range/main.cpp
@@ -5,7 +5,7 @@ int main(void)
 	sr::sr sr; //starting the game
 	sr::player player;
 
-	for(int i=0; i<1000; i++)
+	for(int i=0; i<10000; i++)
 	{
 		player.update();
 		sr.update(player.side_angle, player.up_angle);
