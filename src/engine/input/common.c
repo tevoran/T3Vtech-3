effd016077ecd095f87c940d446a4621f12156af
tevoran <j.pfeiffer93@gmx.de>
2022-04-12 19:26:00 +0200

    removed a keyboard input bug

--- src/engine/input/common.c
+++ src/engine/input/common.c
@@ -6,11 +6,11 @@ void tt_input_update()
 {
 	tt_input_controller_button_reset();
 	tt_input_mouse_button_reset();
+	tt_input_keyboard_update(); 
 	while(SDL_PollEvent(&tt_event))
 	{
 		tt_input_mouse_update(tt_event);
-		tt_input_keyboard_update(); 
-		tt_input_keyboard_press_update(tt_event); //need to be after the keyboard update
+		tt_input_keyboard_press_update(tt_event); //needs to be after the keyboard update
 		tt_input_controller_update(tt_event);
 	}
 }
